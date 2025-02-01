a001d63924f6034738879b58aa2001e65e76d286
SC0FI - Laptop <maddoxjacob64@proton.me>
2025-02-01 16:08:17 -0600

    Testing 3

--- Testing/Testing2.java
+++ Testing/Testing2.java
@@ -3,9 +3,11 @@ package Testing;
 import com.illposed.osc.OSCMessage;
 import com.illposed.osc.transport.OSCPortOut;
 
+import java.awt.*;
 import java.net.InetAddress;
 import java.security.Key;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import javax.swing.*;
@@ -14,7 +16,16 @@ import java.awt.event.*;
 public class Testing2 {
     public static void main(String args[]) {
         try {
-            OSCPortOut sender = new OSCPortOut(InetAddress.getByName("192.168.50.172"), 9000);
+            Scanner input = new Scanner(System.in);
+            String idealIp = input.next();
+            if (Objects.equals(idealIp, "localhost") | Objects.equals(idealIp, "")) {
+                idealIp = "localhost";
+            } else if (Objects.equals(idealIp, "PC")) {
+                idealIp = "192.168.50.172";
+            }
+
+
+            OSCPortOut sender = new OSCPortOut(InetAddress.getByName(idealIp), 9000);
             JFrame frame = new JFrame("Key Press Example");
 
             // KeyListener to detect key press and release
@@ -57,6 +68,33 @@ public class Testing2 {
                         } catch (Exception ex) {
                             ex.printStackTrace();
                         }
+                    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+                        // Send OSC message with integer1 when Right Arrow is pressed
+                        System.out.println("Right Arrow is Pressed, sending 1");
+                        OSCMessage msg = new OSCMessage ("/input/LookRight",Collections.singletonList(1));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+                        // Send OSC message with integer1 when Left Arrow is pressed
+                        System.out.println("Left Arrow is Pressed, sending 1");
+                        OSCMessage msg = new OSCMessage ("/input/LookLeft",Collections.singletonList(1));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                        // Send OSC message with integer 1 when Space is pressed
+                        System.out.println("Space is Pressed, sending 1");
+                        OSCMessage msg = new OSCMessage("/input/Jump", Collections.singletonList(1));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
                     }
                 }
 
@@ -81,7 +119,7 @@ public class Testing2 {
                             ex.printStackTrace();
                         }
                     } else if (e.getKeyCode() == KeyEvent.VK_A) {
-                        // Send OSC message with integer - when A is released
+                        // Send OSC message with integer 0 when A is released
                         System.out.println("A key released, sending 0");
                         OSCMessage msg = new OSCMessage("/input/MoveLeft", Collections.singletonList(0));
                         try {
@@ -90,9 +128,36 @@ public class Testing2 {
                             ex.printStackTrace();
                         }
                     } else if (e.getKeyCode() == KeyEvent.VK_D) {
-                        // Send OSC message with integer - when D is released
+                        // Send OSC message with integer 0 when D is released
                         System.out.println("D key released, sending 0");
                         OSCMessage msg = new OSCMessage("/input/MoveRight", Collections.singletonList(0));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                        // Send OSC message with integer 0 when Right Arrow is released
+                        System.out.println("Right Arrow key released, sending 0");
+                        OSCMessage msg = new OSCMessage("/input/LookRight", Collections.singletonList(0));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                        // Send OSC message with integer 0 when Left Arrow is released
+                        System.out.println("Left Arrow key released, sending 0");
+                        OSCMessage msg = new OSCMessage("/input/LookLeft", Collections.singletonList(0));
+                        try {
+                            sender.send(msg);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                        // Send OSC message with integer 0 when Space is pressed
+                        System.out.println("Space is Released, sending 0");
+                        OSCMessage msg = new OSCMessage("/input/Jump", Collections.singletonList(0));
                         try {
                             sender.send(msg);
                         } catch (Exception ex) {
@@ -102,7 +167,8 @@ public class Testing2 {
                 }
             });
 
-            frame.setSize(300, 300);
+            frame.setSize(600, 600);
+            frame.getContentPane().setBackground(Color.darkGray);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
 
